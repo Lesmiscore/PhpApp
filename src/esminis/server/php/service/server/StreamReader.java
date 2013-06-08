1f842f69d402e73e056d22a3c44caea422ff7c6d
Tautvydas Andrikys <esminis@users.noreply.github.com>
2013-06-08 21:54:37 +0300

    Show error messages from php server Some refactoring

---
+++ src/esminis/server/php/service/server/StreamReader.java
@@ -0,0 +1,33 @@
+package esminis.server.php.service.server;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class StreamReader extends AsyncTask<Object, Void, Void> {
+
+	@Override
+	protected Void doInBackground(Object... arguments) {
+		BufferedReader reader = new BufferedReader(
+			new InputStreamReader((InputStream)arguments[0])
+		);
+		String line = null;
+		for (;;) {
+			try {
+				line = reader.readLine();
+			} catch (IOException ex) {
+				break;
+			}
+			if (line == null) {
+				break;
+			}
+			((Php)arguments[1]).sendErrorLine(line);
+		}
+		((Php)arguments[1]).sendAction("stop");
+		return null;
+	}
+	
+}
