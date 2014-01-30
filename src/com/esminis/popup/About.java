799a8f9c1185c3db9f944486c0d772f27461d448
Tautvydas Andrikys <esminis@users.noreply.github.com>
2014-01-30 22:37:26 +0200

    About dialog with tabs

--- src/com/esminis/popup/About.java
+++ src/com/esminis/popup/About.java
@@ -1,118 +1,107 @@
 package com.esminis.popup;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.os.Message;
 import android.text.Html;
 import android.text.method.ScrollingMovementMethod;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 import com.esminis.server.php.R;
 
-public class About extends DialogFragment {
+public class About extends AlertDialog {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setStyle(STYLE_NO_TITLE, 0);
+	public About(Context context) {
+		super(context);
+		setView(createView());
+		setButton(DialogInterface.BUTTON_NEGATIVE, getContext().getString(R.string.close), (Message)null);
 	}
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state) {
-		final View view = getLayoutInflater(state).inflate(R.layout.about, null);
+	public View createView() {
+		final View view = getLayoutInflater().inflate(R.layout.about, null);
 		if (view == null) {
 			return null;
 		}
-		setMenuVisibility(true);
-		((ViewPager)view.findViewById(R.id.pager)).setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
+		TabHost tabhost = (TabHost)view.findViewById(R.id.tabhost);
+		tabhost.setup();
+		TabHost.TabSpec tab = tabhost.newTabSpec(getContext().getString(R.string.about));
+		tab.setIndicator(tab.getTag());
+		tab.setContent(new TabHost.TabContentFactory() {
 			@Override
-			public int getCount() {
-				return 2;
-			}
-
-			@Override
-			public Fragment getItem(final int i) {
-				return i == 0 ? new AboutFragment() : new LicensesFragment();
-			}
-
-			@Override
-			public CharSequence getPageTitle(int position) {
-				return getString(position == 1 ? R.string.licenses : R.string.about);
+			public View createTabContent(String tag) {
+				return createMain((ViewGroup)view.findViewById(android.R.id.tabcontent));
 			}
 		});
+		tabhost.addTab(tab);
+		tab = tabhost.newTabSpec(getContext().getString(R.string.licenses));
+		tab.setIndicator(tab.getTag());
+		tab.setContent(new TabHost.TabContentFactory() {
+			@Override
+			public View createTabContent(String tag) {
+				return createLicenses((ViewGroup)view.findViewById(android.R.id.tabcontent));
+			}
+		});
+		tabhost.addTab(tab);
 		return view;
 	}
 
-
-	static public class AboutFragment extends Fragment {
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state) {
-			TextView view = (TextView)inflater.inflate(R.layout.about_main, container, false);
-			if (view != null) {
-				view.setText(Html.fromHtml(getString(R.string.about_content)));
-				view.setMovementMethod(new ScrollingMovementMethod());
-			}
-			return view;
+	private View createMain(ViewGroup container) {
+		TextView view = (TextView)getLayoutInflater().inflate(R.layout.about_main, container, false);
+		if (view != null) {
+			view.setText(Html.fromHtml(getContext().getString(R.string.about_content)));
+			view.setMovementMethod(new ScrollingMovementMethod());
 		}
-
+		return view;
 	}
 
-	static public class LicensesFragment extends Fragment {
-
-		@SuppressLint("SetJavaScriptEnabled")
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state) {
-			final View view = inflater.inflate(R.layout.about_licenses, container, false);
-			if (view == null) {
-				return null;
-			}
-			WebView htmlView = (WebView)view.findViewById(R.id.text);
-			if (htmlView == null) {
-				return null;
-			}
-			final Handler handler = new Handler();
-			htmlView.getSettings().setJavaScriptEnabled(true);
-			htmlView.setWebViewClient(new WebViewClient() {
-
-				@Override
-				public void onPageFinished(WebView v, String url) {
-					handler.postDelayed(
-						new Runnable() {
-							public void run() {
-								view.findViewById(R.id.text).setVisibility(View.VISIBLE);
-								view.findViewById(R.id.preloader).setVisibility(View.GONE);
-								view.findViewById(R.id.preloader_container).setVisibility(View.GONE);
-							}
-						}, 1500
-					);
-				}
-
-				@Override
-				public boolean shouldOverrideUrlLoading(WebView v, String url) {
-					if (v == null || v.getContext() == null) {
-						return false;
-					}
-					v.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
-					return true;
-				}
-
-			});
-			htmlView.loadUrl("file:///android_asset/Licenses.html");
-			return view;
+	@SuppressLint("SetJavaScriptEnabled")
+	private View createLicenses(ViewGroup container) {
+		final View view = getLayoutInflater().inflate(R.layout.about_licenses, container, false);
+		if (view == null) {
+			return null;
 		}
+		WebView htmlView = (WebView)view.findViewById(R.id.text);
+		if (htmlView == null) {
+			return null;
+		}
+		final Handler handler = new Handler();
+		htmlView.getSettings().setJavaScriptEnabled(true);
+		htmlView.setWebViewClient(new WebViewClient() {
 
+			@Override
+			public void onPageFinished(WebView v, String url) {
+				handler.postDelayed(
+					new Runnable() {
+							public void run() {
+							view.findViewById(R.id.text).setVisibility(View.VISIBLE);
+							view.findViewById(R.id.preloader).setVisibility(View.GONE);
+							view.findViewById(R.id.preloader_container).setVisibility(View.GONE);
+						}
+						}, 1500
+				);
+			}
+
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView v, String url) {
+				if (v == null || v.getContext() == null) {
+					return false;
+				}
+				v.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+				return true;
+			}
+		});
+		htmlView.loadUrl("file:///android_asset/Licenses.html");
+		return view;
 	}
 
 }
