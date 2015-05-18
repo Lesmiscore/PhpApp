19ded3550a15806911cc63c7b03fa1b993d983b6
Tautvydas Andrikys <esminis@esminis.lt>
2015-05-18 22:10:25 +0300

    Added new php modules Updated php arm binaries

--- app/src/main/assets/www/index.php
+++ app/src/main/assets/www/index.php
@@ -1,1 +1,13 @@
-<?php phpinfo(); ?>
+<?php
+
+$path = dirname(__FILE__) . '/test.sqlite';
+$path = '/data/data/com.esminis.server.php/files/test.sqlite';
+
+file_put_contents($path, '');
+var_dump(is_file($path));
+var_dump(chmod($path, 0777));
+var_dump(is_executable($path));
+
+odbc_connect('Driver=SQLite3;Database=' . $path, '', '', SQL_CUR_USE_ODBC);
+
+//phpinfo(); ?>
