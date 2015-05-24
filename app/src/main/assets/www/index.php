a3b2aa994a6b537cf4daa8208fcc22d0e8c3f448
Tautvydas Andrikys <esminis@esminis.lt>
2015-05-24 16:43:08 +0300

    PHP 5.6.9 x86 binaries

--- app/src/main/assets/www/index.php
+++ app/src/main/assets/www/index.php
@@ -1,13 +1,1 @@
-<?php
-
-$path = dirname(__FILE__) . '/test.sqlite';
-$path = '/data/data/com.esminis.server.php/files/test.sqlite';
-
-file_put_contents($path, '');
-var_dump(is_file($path));
-var_dump(chmod($path, 0777));
-var_dump(is_executable($path));
-
-odbc_connect('Driver=SQLite3;Database=' . $path, '', '', SQL_CUR_USE_ODBC);
-
-//phpinfo(); ?>
+<?php phpinfo(); ?>
