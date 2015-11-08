8c00ecd293919700385050f82dd41e8baecd4d4b
Tautvydas Andrikys <Tautvydas Andrikys@localhost>
2015-11-08 11:43:01 +0200

    Removed temp files

--- MariaDb/src/arm/obj/local/armeabi/objs/filter/filter.o.d
+++
@@ -1,2 +0,0 @@
-/home/tautvydas/Documents/Development/Java/ServerForPhp/MariaDb/src/arm/obj/local/armeabi/objs/filter/filter.o: \
- /home/tautvydas/Documents/Development/Java/ServerForPhp/MariaDb/src/arm/jni/filter.c
