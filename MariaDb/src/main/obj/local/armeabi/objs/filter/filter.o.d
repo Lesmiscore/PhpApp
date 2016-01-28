d47a2e164a53fc71a5e5c76747fce5a1bfcda9d9
Tautvydas Andrikys <Tautvydas Andrikys@localhost>
2016-01-28 23:04:16 +0200

    Preparation for packages download

---
+++ MariaDb/src/main/obj/local/armeabi/objs/filter/filter.o.d
@@ -0,0 +1,2 @@
+/home/tautvydas/Documents/Development/Java/Server/MariaDb/src/main/obj/local/armeabi/objs/filter/filter.o: \
+ /home/tautvydas/Documents/Development/Java/Server/MariaDb/src/main/jni/filter.c
---
+++ Php/src/main/obj/local/armeabi/objs/filter/filter.o.d
@@ -0,0 +1,2 @@
+/home/tautvydas/Documents/Development/Java/Server/Php/src/main/obj/local/armeabi/objs/filter/filter.o: \
+ /home/tautvydas/Documents/Development/Java/Server/Php/src/main/jni/filter.c
