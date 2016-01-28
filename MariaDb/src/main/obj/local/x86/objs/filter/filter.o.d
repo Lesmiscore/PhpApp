5a06341dafab9aae045bf88136cfe60e31068076
Tautvydas Andrikys <esminis@users.noreply.github.com>
2016-01-28 23:04:16 +0200

    Preparation for packages download

---
+++ MariaDb/src/main/obj/local/x86/objs/filter/filter.o.d
@@ -0,0 +1,2 @@
+/home/tautvydas/Documents/Development/Java/Server/MariaDb/src/main/obj/local/x86/objs/filter/filter.o: \
+ /home/tautvydas/Documents/Development/Java/Server/MariaDb/src/main/jni/filter.c
---
+++ Php/src/main/obj/local/x86/objs/filter/filter.o.d
@@ -0,0 +1,2 @@
+/home/tautvydas/Documents/Development/Java/Server/Php/src/main/obj/local/x86/objs/filter/filter.o: \
+ /home/tautvydas/Documents/Development/Java/Server/Php/src/main/jni/filter.c
