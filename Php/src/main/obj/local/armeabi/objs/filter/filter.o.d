24f95d5864655c3d1ff6b58be37be0ec6fe10aa6
Tautvydas Andrikys <Tautvydas Andrikys@localhost>
2016-02-27 09:55:12 +0200

    Version updated

--- Php/src/main/obj/local/armeabi/objs/filter/filter.o.d
+++
@@ -1,2 +0,0 @@
-/home/tautvydas/Documents/Development/Java/Server/Php/src/main/obj/local/armeabi/objs/filter/filter.o: \
- /home/tautvydas/Documents/Development/Java/Server/Php/src/main/jni/filter.c
